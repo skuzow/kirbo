38e831bd215bb01b12f43cf5fffde425b3f6ef74
skuzow <alex@skuzow.net>
2023-06-30 10:35:59 +0200

    feat: prod & dev env

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello world!');
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { config } from 'dotenv';
+import { resolve } from 'path';
+
+const envFile = process.env.NODE_ENV === 'development' ? '.dev.env' : '.env';
+const envFilePath = resolve(process.cwd(), envFile);
+config({ path: envFilePath });
+
+// eslint-disable-next-line no-console
+console.log(process.env.PRINT);
