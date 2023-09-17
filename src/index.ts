f015c6f95cf7af191355c1a52ea1446774f110a1
skuzow <alex@skuzow.net>
2023-09-17 02:49:26 +0200

    feat: mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import ExtendedClient from './client/ExtendedClient.js';
 import { processEnv } from './systems/config.js';
+import { connectDB } from './systems/mongo.js';
 
 processEnv();
+await connectDB();
 export const client = new ExtendedClient();
 client.start();
