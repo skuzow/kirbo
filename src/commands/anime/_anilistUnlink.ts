8339f93ac12a840104f02ce2423c38816b5b6a09
skuzow <alex@skuzow.net>
2023-09-25 21:03:43 +0200

    feat: anilist unlink command

--- src/commands/anime/_anilistUnlink.ts
+++ src/commands/anime/_anilistUnlink.ts
@@ -1,8 +1,14 @@
 import { ChatInputCommandInteraction, CacheType } from 'discord.js';
+import { existUser, deleteUser } from '../../services/userService.js';
 
 export async function processUnlinkQuery(
   interaction: ChatInputCommandInteraction<CacheType>
 ) {
-  // TODO: Implement unlinking
-  await interaction.reply('Unlinking is not implemented yet');
+  const discordId: string = interaction.user.id;
+  if (await existUser(discordId)) {
+    await deleteUser(discordId);
+    await interaction.reply(`Discord account successfully unlinked!`);
+  } else {
+    await interaction.reply(`Your discord account is not linked yet!`);
+  }
 }
