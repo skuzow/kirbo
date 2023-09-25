e8a77ad5b0ad7a9d2d59f926b582db2248399da5
skuzow <alex@skuzow.net>
2023-09-25 21:51:00 +0200

    feat: check already linked user anilist

--- src/commands/anime/_anilistLink.ts
+++ src/commands/anime/_anilistLink.ts
@@ -14,18 +14,29 @@ import {
   requestAnilistQuery
 } from './_anilistUtils.js';
 import { insertUserAiringAnimesData } from '../../services/airingAnimeService.js';
-import { getOrCreateUser } from '../../services/userService.js';
+import {
+  existUser,
+  getOrCreateUser,
+  getUser
+} from '../../services/userService.js';
 
 export async function processLinkQuery(
   interaction: ChatInputCommandInteraction<CacheType>
 ) {
   try {
-    const username = String(interaction.options.get('username')?.value);
-    const response: Anilist = await requestAnilistQuery(username);
     const discordId: string = interaction.user.id;
-    await insertUserData(response, discordId);
-    const infoEmbed: EmbedBuilder = generateInfoEmbed(response);
-    await interaction.reply({ embeds: [infoEmbed] });
+    if (await existUser(discordId)) {
+      const user: HydratedDocument<IUser> | null = await getUser(discordId);
+      await interaction.reply(`\
+        Your discord account is already linked with ${user?.anilistUsername} Anilist. If you want to unlink use \`/anilist unlink\` command!
+      `);
+    } else {
+      const username = String(interaction.options.get('username')?.value);
+      const response: Anilist = await requestAnilistQuery(username);
+      await insertUserData(response, discordId);
+      const infoEmbed: EmbedBuilder = generateInfoEmbed(response);
+      await interaction.reply({ embeds: [infoEmbed] });
+    }
   } catch (e) {
     console.error(e);
     await interaction.reply(`Username doesn't exist in Anilist`);
